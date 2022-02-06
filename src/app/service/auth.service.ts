8c302db997e12001ed2283bc95795eb1c7bc2ab9
Rubem <rubem.martinsf@gmail.com>
2022-02-06 01:19:35 -0300

    deploy front-end netlify

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -21,15 +21,15 @@ export class AuthService {
   }
   
   entrar(userLogin: UserLogin):Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>(' https://blogpessoalrubem.herokuapp.com/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+    return this.http.post<User>(' https://blogpessoalrubem.herokuapp.com/usuarios/cadastrar', user)
   }
 
   getByIdUser(id: number): Observable<User>{
-    return this.http.get<User>(`http://localhost:8080/usuarios/${id}`, this.token)
+    return this.http.get<User>(` https://blogpessoalrubem.herokuapp.com/usuarios/${id}`, this.token)
   }
 
   logado(){
